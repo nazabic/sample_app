bb4b645bc8117817c1717c8a30591642a9428e0b
Diana Bicazan <diana.bicazan@reevoo.com>
2014-03-31 10:25:31 +0100

    Finish Chapter 3: Static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+    it "should have the content 'Home'" do
+          visit '/home'
+          expect(page).to have_content('Home')
+    end
+    it "should have the title 'Home'" do
+          visit '/home'
+          expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
+  end
+  describe "Help page" do
+    it "should have the content 'Help'" do
+          visit '/help'
+          expect(page).to have_content('Help')
+    end
+    it "should have the title 'Help'" do
+          visit '/help'
+          expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    end
+  end
+  describe "About page" do
+    it "should have the content 'About Us'" do
+          visit '/about'
+          expect(page).to have_content('About Us')
+    end
+    it "should have the title 'About Us'" do
+          visit '/about'
+          expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+    end
+  end
+end
+
